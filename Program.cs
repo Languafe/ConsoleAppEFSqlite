5ab4a3033a289f63bf24e43e0647b2812a3e68db
Erik Langhaug <erik.langhaug@outlook.com>
2019-07-28 19:12:11 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ConsoleApp
+{
+    class Thing
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlite("Data Source=app.db");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+
+
+    class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Thing> Things { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite("Data Source=app.db")
+                .Options;
+
+            var dbContext = new AppDbContext(options);
+
+            dbContext.Database.Migrate();
+
+            dbContext.Things.AddRange(new [] {
+                new Thing { Title = "Thing" },
+            });
+
+            dbContext.SaveChanges();
+
+            var things = dbContext.Things.ToList();
+            foreach (var thing in things)
+            {
+                Console.WriteLine($"Id: {thing.Id}, Title: {thing.Title}");
+            }
+        }
+    }
+}
